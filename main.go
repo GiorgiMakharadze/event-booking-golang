8078b4b4eea04d00dd94c6bfe5950ab5a0881c67
Gi0rgiMakharadze <makharadzegiorgi00@gmail.com>
2024-08-22 02:00:55 +0400

    type event

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/GiorgiMakharadze/event-booking-golang/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,12 +11,26 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
-
+	server.POST("/events", createEvent)
 	server.Run(":8080")
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "events",
-	})
+	events := models.GetAllEvents()
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	ctx.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
 }
