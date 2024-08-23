1a46afe20f088067a209d080ce4ac1a806be11b9
Gi0rgiMakharadze <makharadzegiorgi00@gmail.com>
2024-08-24 02:03:51 +0400

    update event

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"github.com/GiorgiMakharadze/event-booking-golang/db"
-	"github.com/GiorgiMakharadze/event-booking-golang/models"
+	"github.com/GiorgiMakharadze/event-booking-golang/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,56 +11,8 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080")
 
-}
-
-func getEvents(ctx *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events, Try again later"})
-		return
-	}
-	ctx.JSON(http.StatusOK, events)
-}
-
-func getEvent(ctx *gin.Context) {
-	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not pasrse event id"})
-		return
-	}
-	event, err := models.GetEventByID(eventId)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event, Try again later"})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, event)
-
-}
-
-func createEvent(ctx *gin.Context) {
-	var event models.Event
-	err := ctx.ShouldBindJSON(&event)
-
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events, Try again later"})
-		return
-	}
-
-	ctx.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
 }
---
+++ routes/events.go
@@ -0,0 +1,86 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/GiorgiMakharadze/event-booking-golang/models"
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(ctx *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events, Try again later"})
+		return
+	}
+	ctx.JSON(http.StatusOK, events)
+}
+
+func getEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not pasrse event id"})
+		return
+	}
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event, Try again later"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, event)
+
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events, Try again later"})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"message": "event created!", "event": event})
+}
+
+func updateEvent(ctx *gin.Context) {
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not pasrse event id"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event, Try again later"})
+		return
+	}
+
+	var updateEvent models.Event
+	err = ctx.ShouldBindJSON(&updateEvent)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request"})
+		return
+	}
+
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not update event, Try again later"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "event updated!", "event": updateEvent})
+
+}
