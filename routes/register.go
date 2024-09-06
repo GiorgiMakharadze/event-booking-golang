45f8971ff2b3edf953e1dac903a84c01518993b2
Gi0rgiMakharadze <makharadzegiorgi00@gmail.com>
2024-09-06 15:01:58 +0400

    register for event

---
+++ routes/register.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/GiorgiMakharadze/event-booking-golang/models"
+	"github.com/gin-gonic/gin"
+)
+
+func registerForEvent(ctx *gin.Context) {
+	userId := ctx.GetInt64("userId")
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "could not pasrse event id"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch event, Try again later"})
+	}
+	
+	err = event.Register(userId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "could not register user for event, Try again later"})
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "user registered for event", "event": event})
+}
+
+func cancelRegistration(ctx *gin.Context) {
+
+}
